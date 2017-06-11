718d9d48a53fc28617d272211d55f5f8ee646827
Kishan Gajjar <gajjar.kishan92@gmail.com>
2017-06-11 02:38:21 -0400

    built search_bar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,1 +1,28 @@
+import React, {Component} from 'react';
 
+// SearchBar Component
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {term: ''}; //store input keyword in state
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={(event) => this.onInputChange(event.target.value)} />
+      </div>
+    );
+  }
+
+  //function to capture input keyword in state and pass to callback
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+};
+
+export default SearchBar;
